dc78795ad56e1c18e194c689bf4cd45b294f04c6
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-08 09:40:51 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap3espg/autoescola3espg/AutoEscola3EspgApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap3espg.autoescola3espg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoEscola3EspgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoEscola3EspgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap3espg/autoescola3espg/AutoEscola3EspgApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap3espg.autoescola3espg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoEscola3EspgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
